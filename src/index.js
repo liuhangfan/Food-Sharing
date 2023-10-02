249821075b513755933925c2e263ff52e2be97fb
liu hangfan <liuhangfan@lius-MacBook-Pro.local>
2023-10-01 21:39:21 -0700

    add home page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import HomePage from './pages/HomePage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const AppBase = () => (
+  <Switch>
+    <Route
+      exact
+      path={ROUTES.LANDING}
+      render={() => (
+        // <WithFooter>
+        //   <WithNavbar>
+            <HomePage />
+        //   </WithNavbar>
+        // </WithFooter>
+      )}
+    />
+    <Route
+      path={ROUTES.PASSWORD_FORGET}
+    />
+    <Route
+      path={ROUTES.ACCOUNT}
+    />
+    <Route
+      path={ROUTES.FOODS}
+    />
+    <Route
+      path={ROUTES.FOOD_DETAIL}
+    />
+    <Route
+      path={ROUTES.ADD_FOOD}
+    />
+    <Route
+      path={ROUTES.MY_FOOD_HISTORY}
+    />
+    <Route
+      path={ROUTES.SIGN_UP}
+    />
+    <Route
+      path={ROUTES.LOG_IN}
+    />
+    <Route
+      path={ROUTES.EDIT_PROFILE}
+    />
+    <Route
+      path={ROUTES.SETTINGS}
+    />
+  </Switch>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <BrowserRouter>
+    <AppBase />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,3 +6,101 @@ import {Link} from 'react-router-dom';
 import * as ROUTES from '../constants/routes.js';
 
 import {withFirebase} from '../components/Firebase';
+
+
+const HomePage = props => (
+    <div className="home-page">
+      <div className="home-page-banner">
+        <div className="home-banner-txt-container">
+          <div className="inner">
+            <h1>Why should you join Food Sharing</h1>
+  
+            <div className="inner-content">
+              Food Sharing is one of the world's food sharing platforms. Feeding hungry people through food recovery is the best use for surplus food. Food Sharing aimed at efficiently redistributing surplus food to those who need it the most.
+              If you want to contribute for a sustainable environment and mitigate the food waste issue, Food Sharing is for you.
+            </div>
+  
+            <div className="banner-buttons">
+              <Link to={ROUTES.SIGN_UP} className="btn btn-black btn-homepage">
+                <span>Join Now</span>
+              </Link>
+              <Link to={ROUTES.FOODS} className="btn btn-find btn-homepage">
+                <span>Find Foods</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <img
+          className="home-img-banner"
+          src={require('../images/food.png')}
+          alt="Food homepage banner"
+        />
+      </div>
+  
+      <div className="homepage-cards">
+        <div className="inner-card">
+          <h2>Secure</h2>
+          <div className="line-break" />
+          <span>
+            We ensure that every transaction is secure with a compliance
+            procedure.
+          </span>
+        </div>
+        <div className="inner-card">
+          <h2>Easy</h2>
+          <div className="line-break" />
+          <span>
+            Find the sharing food you most need, request to owner, and decide on when you can
+            come pick it up.
+          </span>
+        </div>
+        <div className="inner-card">
+          <h2>Efficient</h2>
+          <div className="line-break" />
+          <span>
+            Track your request without hassle. You will be notified for every
+            process change.
+          </span>
+        </div>
+        <div className="inner-card">
+          <h2>Search</h2>
+          <div className="line-break" />
+          <span>
+           We offer a wide variety of food options conveniently located near you.
+          </span>
+        </div>
+      </div>
+  
+      <div className="title-section">How does Food Sharing work? </div>
+      <div className="how-it-works-container">
+        <img
+          className="mockup-platform"
+          src={require('../images/foodSaver.png')}
+          alt="Food stack homepage banner"
+        />
+  
+        <div className="how-it-works">
+          <div>
+            <div className="header-title">Find your food</div>
+            Search the sharing food that you need. We provide a a wide variety of food options.
+          </div>
+          <div>
+            <div className="header-title">Request the food</div>
+            Request to get the food from the food's owner. We let the
+            owner decide to give their surplus foods away.
+          </div>
+          <div>
+            <div className="header-title">Meet the owner</div>
+            Set the appointment to meet the owner to pickup the food. Our platform
+            will only give the owner detail location once the request is approved.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  
+  HomePage.propTypes = {
+    firebase: PropTypes.object
+  };
+  
+  export default HomePage;
