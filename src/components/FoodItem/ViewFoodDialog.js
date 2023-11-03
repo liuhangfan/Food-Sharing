25f4e8fbf0ab66f2f8e9448d747ef1f8a01111b2
liu hangfan <liuhangfan@lius-MacBook-Pro.local>
2023-11-03 00:32:12 -0700

    add query email

--- src/components/FoodItem/ViewFoodDialog.js
+++ src/components/FoodItem/ViewFoodDialog.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Avatar, Button, Dialog, DialogActions, DialogContent, Stack, TextField, Typography } from '@mui/material';
+import { Avatar, Button, Dialog, DialogActions, DialogContent, Stack, TextField, Typography, Alert } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -15,11 +15,48 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
 import CardMedia from '@mui/material/CardMedia';
 import Card from '@mui/material/Card';
+import { getUserEmailByUID } from '../Firebase/auth';
+import CopyToClipboard from "react-copy-to-clipboard";
+import LoadingButton from '@mui/lab/LoadingButton';
+import SearchIcon from '@mui/icons-material/Search';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 const ViewFoodDialog = (props) => {
+  const [showContact, setShowContactAlert] = useState(null);
+  const [showContactSuccess, setShowContactSuccessAlert] = useState("success");
+  const [loading, setLoading] = useState(false);
+  const [requestDisable, setRequestDisable] = useState(false);
+
   if(props.food === null) return null;
+
+
+  const handleGetEmail = async (UID) => {
+    setLoading(true);
+    try {
+        const userEmail = await getUserEmailByUID(UID);
+        setShowContactSuccessAlert("success");
+        setShowContactAlert(userEmail);
+        setRequestDisable(true);
+      } catch (error) {
+        console.error('Error:', error);
+        setShowContactAlert("Something wrong, try it later");
+        setShowContactSuccessAlert("warning")
+      }
+    setLoading(false);
+  }
+  
+  const reset = () => {
+    setShowContactAlert(null)
+    setShowContactSuccessAlert("success");
+    setLoading(false);
+    setRequestDisable(false);
+  }
+
   return (
     <Dialog
-      onClose={props.onCloseDialog}
+      onClose={() => {
+        reset()
+        props.onCloseDialog()
+      }}
       open={props.showDialog}
       maxWidth="md"
       fullWidth={true}
@@ -84,17 +121,33 @@ const ViewFoodDialog = (props) => {
           <Typography>{formFields.fileName}</Typography>
         </Stack> */}
       </DialogContent>
-      
+      {showContact && <Alert severity={showContactSuccess} onClose={() => {
+                                            setShowContactAlert(null)
+                                            setRequestDisable(false);
+                                        }}>
+                         {/* {showContact} */}
+                         {<CopyToClipboard
+                    text={showContact}>
+                        {/* <Button variant="text" size="small" endIcon={<ContentCopyIcon />}>
+                            {showContact}
+                        </Button> */}
+                        <span>{showContact}</span>
+                    </CopyToClipboard>}
+                    </Alert>}
       <DialogActions>
-        {/* {isSubmitting ?
-          <Button color="secondary" variant="contained" disabled={true}>
-            Submitting...
-          </Button> : */}
-          <Button color="secondary" variant="contained">
-            Request Contact
-          </Button>
+          <LoadingButton
+          size="small"
+          onClick={() => handleGetEmail(props.food.uid)}
+          endIcon={<SearchIcon />}
+          loading={loading}
+          loadingPosition="end"
+          variant="contained"
+          disabled={requestDisable}
+        >
+          <span>Request Contact</span>
+        </LoadingButton>
       </DialogActions>
-
+      
     </Dialog>
   );
 };
--- src/pages/AllFoodsPage.js
+++ src/pages/AllFoodsPage.js
@@ -14,114 +14,113 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
 
-  const AllFoodsPage =() => {
+const AllFoodsPage =() => {
+
+  const [foods, setFoods] = useState([]);
+  const [isLoadingFoods, setIsLoadingFoods] = useState(true);
+  const [currentFood, setCurrentFood] = useState(null);
   
-    const [foods, setFoods] = useState([]);
-    const [isLoadingFoods, setIsLoadingFoods] = useState(true);
-    const [currentFood, setCurrentFood] = useState(null);
-    
-    const [location, setLocation] = useState({
-      lat: 0,
-      lon: 0
-    });
+  const [location, setLocation] = useState({
+    lat: 0,
+    lon: 0
+  });
+  
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
+          });
+        },
+        (error) => {
+          setLocation({
+            lat: 0,
+            lon: 0
+          });
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by your browser.');
+      setLocation({
+        lat: 0,
+        lon: 0
+      });
+    }
+  }, []);
 
-    useEffect(() => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setLocation({
-              lat: position.coords.latitude,
-              lon: position.coords.longitude
-            });
-          },
-          (error) => {
-            setLocation({
-              lat: 0,
-              lon: 0
-            });
-            console.error('Error getting location:', error);
-          }
-        );
-      } else {
-        console.error('Geolocation is not supported by your browser.');
-        setLocation({
-          lat: 0,
-          lon: 0
-        });
-      }
-    }, []);
+  //Get foods once user is logged in
+  useEffect(() => {
+      getFoodsByGeo(setFoods, setIsLoadingFoods, 50 * 1000, [location["lat"],location["lon"]]);
+  }, [location])
 
+    return (
+      <div>
 
-    //Get foods once user is logged in
-    useEffect(() => {
-        getFoodsByGeo(setFoods, setIsLoadingFoods, 50 * 1000, [location["lat"],location["lon"]]);
-    }, [location])
-
-      return (
-        <div>
-
-        <Container>
-            <Stack direction="row" sx={{ paddingTop: "1.5em" }}>
-            <Typography variant="h4" sx={{ lineHeight: 2, paddingRight: "0.5em" }}>
-                All Sharing FOODS:
-            </Typography>
-            </Stack>
-            <Grid container spacing={2}>
-                {foods.map((food) => (
-                  <Grid item key={food.id} xs={12} sm={6} md={4}>
-                    <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-                    <div style={{
-                          paddingTop: '75%', // 4:3 aspect ratio (adjust as needed)
-                          position: 'relative',
-                          overflow: 'hidden',
-                        }}
-                      >
-                      <CardMedia
-                        component="img"
-                        alt={food.title}
-                        
-                        image={food.imageUrl}
+      <Container>
+          <Stack direction="row" sx={{ paddingTop: "1.5em" }}>
+          <Typography variant="h4" sx={{ lineHeight: 2, paddingRight: "0.5em" }}>
+              All Sharing FOODS:
+          </Typography>
+          </Stack>
+          <Grid container spacing={2}>
+              {foods.map((food) => (
+                <Grid item key={food.id} xs={12} sm={6} md={4}>
+                  <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                  <div style={{
+                        paddingTop: '75%', // 4:3 aspect ratio (adjust as needed)
+                        position: 'relative',
+                        overflow: 'hidden',
+                      }}
+                    >
+                    <CardMedia
+                      component="img"
+                      alt={food.title}
+                      
+                      image={food.imageUrl}
+                      style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',
+                      }}
+                    />
+                    </div>
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {food.title}
+                      </Typography>
+                      <TextareaAutosize
+                        maxRows={3}
+                        value={food.description}
+                        readOnly
                         style={{
-                          position: 'absolute',
-                          top: 0,
-                          left: 0,
                           width: '100%',
-                          height: '100%',
-                          objectFit: 'cover',
-                        }}
-                      />
-                      </div>
-                      <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography gutterBottom variant="h5" component="div">
-                          {food.title}
+                          resize: 'vertical',
+                          overflowY: 'auto',
+                        }} />
+                        <Typography>
+                          {food.distance} miles
                         </Typography>
-                        <TextareaAutosize
-                          maxRows={3}
-                          value={food.description}
-                          readOnly
-                          style={{
-                            width: '100%',
-                            resize: 'vertical',
-                            overflowY: 'auto',
-                          }} />
-                          <Typography>
-                            {food.distance} miles
-                          </Typography>
-                      </CardContent>
-                      <CardActions>
-                        <Button size="small" onClick={() => setCurrentFood(food)}>Learn More</Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-        </Container>
-        <ViewFoodDialog food={currentFood}
-                        showDialog={currentFood != null}
-                        onCloseDialog={() => setCurrentFood(null)}>
-        </ViewFoodDialog>
-        </div>
-    )
-  }
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" onClick={() => setCurrentFood(food)}>Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+      </Container>
+      <ViewFoodDialog food={currentFood}
+                      showDialog={currentFood != null}
+                      onCloseDialog={() => setCurrentFood(null)}>
+      </ViewFoodDialog>
+      </div>
+  )
+}
 
 export default AllFoodsPage;
